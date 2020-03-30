b6b917903ea878ce353800cde149eebfbf7332e8
millerb08 <miller.steffens@gmail.com>
2020-03-30 18:26:03 -0500

    change details and functions.php for more scure SQL statemant

--- details.php
+++ details.php
@@ -1,28 +1,99 @@
 <?php 
-include("inc/functions.php");
-$catalog = full_catalog_array();
 
-$pageTitle = "Personal Media Library";
+include("inc/functions.php");
+
+if (isset($_GET["id"])) {
+    $id =  filter_input(INPUT_GET,"id",FILTER_SANITIZE_NUMBER_INT);
+    $item = single_item_array($id);
+  var_dump($item);
+}
+
+if (!isset($item)) {
+    header("location:catalog.php");
+    exit;
+}
+
+$pageTitle = $item["title"];
 $section = null;
 
 include("inc/header.php"); ?>
-		<div class="section catalog random">
 
-			<div class="wrapper">
+<div class="section page">
 
-				<h2>May we suggest something?</h2>
+    <div class="wrapper">
+        
+        <div class="breadcrumbs">
+            <a href="catalog.php">Full Catalog</a>
+            &gt; <a href="catalog.php?cat=<?php echo strtolower($item["category"]); ?>">
+            <?php echo $item["category"]; ?></a>
+            &gt; <?php echo $item["title"]; ?>
+        </div>
+        
+        <div class="media-picture">
+    
+        <span>
+            <img src="<?php echo $item["img"]; ?>" alt="<?php echo $item["title"]; ?>" />
+        </span>
+            
+        </div>
+        
+        <div class="media-details">
+        
+            <h1><?php echo $item["title"]; ?></h1>
+            <table>
+            
+                <tr>
+                    <th>Category</th>
+                    <td><?php echo $item["category"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Genre</th>
+                    <td><?php echo $item["genre"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Format</th>
+                    <td><?php echo $item["format"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Year</th>
+                    <td><?php echo $item["year"]; ?></td>
+                </tr>
+                <?php if (strtolower($item["category"]) == "books") { ?>
+                <tr>
+                    <th>Authors</th>
+                    <td><?php echo implode(", ",$item["authors"]); ?></td>
+                </tr>
+                <tr>
+                    <th>Publisher</th>
+                    <td><?php echo $item["publisher"]; ?></td>
+                </tr>
+                <tr>
+                    <th>ISBN</th>
+                    <td><?php echo $item["isbn"]; ?></td>
+                </tr>    
+                <?php } else if (strtolower($item["category"]) == "movies") { ?>
+                <tr>
+                    <th>Director</th>
+                    <td><?php echo $item["director"]; ?></td>
+                </tr>
+                <tr>
+                    <th>Writers</th>
+                    <td><?php echo implode(", ",$item["writers"]); ?></td>
+                </tr>
+                <tr>
+                    <th>Stars</th>
+                    <td><?php echo implode(", ",$item["stars"]); ?></td>
+                </tr>
+                <?php } else if (strtolower($item["category"]) == "music") { ?>
+                <tr>
+                    <th>Artist</th>
+                    <td><?php echo $item["artist"]; ?></td>
+                </tr>
+                <?php } ?>
+            </table>
+        
+        </div>
+    
+    </div>
 
-        <ul class="items">
-            <?php
-            $random = array_rand($catalog,4);
-            foreach ($random as $id) {
-                echo get_item_html($id,$catalog[$id]);
-            }
-            ?>							
-				</ul>
-
-			</div>
-
-		</div>
-
-<?php include("inc/footer.php"); ?>
+</div>
--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@
 function full_catalog_array(){
   include("connection.php");
   try{
-    $results = $db->query("SELECT title, category,img FROM Media");
+    $results = $db->query("SELECT media_id, title, category,img FROM Media");
   }catch(Exception $e){
     echo "Unable to retrieved results";
     exit;
@@ -12,9 +12,26 @@ function full_catalog_array(){
   $catalog = $results->fetchAll();
   return $catalog;
 }
+function single_item_array($id){
+  include("connection.php");
+  try{
+    $results = $db->prepare("SELECT title, category,img,format,year, genre, publisher, isbn FROM Media 
+    JOIN Genres ON Media.genre_id = Genres.genre_id 
+    LEFT OUTER JOIN Books ON Media.media_id = Books.media_id 
+    Where Media.media_id = ?");
+    $results->bindParam(1,$id,PDO::PARAM_INT);
+    $results->execute();
+  }catch(Exception $e){
+    echo "Unable to retrieved results";
+    exit;
+  }
+  //FETCH DATA ASSOCIATIVE
+  $catalog = $results->fetch();
+  return $catalog;
+}
 function get_item_html($id,$item) {
     $output = "<li><a href='details.php?id="
-        . $id . "'><img src='" 
+        . $item["media_id"] . "'><img src='" 
         . $item["img"] . "' alt='" 
         . $item["title"] . "' />" 
         . "<p>View Details</p>"
