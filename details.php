8ce5c8267a2571e629cfad4d07c3de5206615a6d
millerb08 <miller.steffens@gmail.com>
2020-03-30 19:29:57 -0500

    added athors , artists and stars to detailed page.

--- details.php
+++ details.php
@@ -5,10 +5,9 @@ include("inc/functions.php");
 if (isset($_GET["id"])) {
     $id =  filter_input(INPUT_GET,"id",FILTER_SANITIZE_NUMBER_INT);
     $item = single_item_array($id);
-  var_dump($item);
 }
 
-if (!isset($item)) {
+if (empty($item)) {
     header("location:catalog.php");
     exit;
 }
@@ -61,7 +60,7 @@ include("inc/header.php"); ?>
                 <?php if (strtolower($item["category"]) == "books") { ?>
                 <tr>
                     <th>Authors</th>
-                    <td><?php echo implode(", ",$item["authors"]); ?></td>
+                    <td><?php echo implode(", ",$item["author"]); ?></td>
                 </tr>
                 <tr>
                     <th>Publisher</th>
@@ -74,20 +73,20 @@ include("inc/header.php"); ?>
                 <?php } else if (strtolower($item["category"]) == "movies") { ?>
                 <tr>
                     <th>Director</th>
-                    <td><?php echo $item["director"]; ?></td>
+                    <td><?php echo implode(", ",$item["director"]); ?></td>
                 </tr>
                 <tr>
                     <th>Writers</th>
-                    <td><?php echo implode(", ",$item["writers"]); ?></td>
+                    <td><?php echo implode(", ",$item["writer"]); ?></td>
                 </tr>
                 <tr>
                     <th>Stars</th>
-                    <td><?php echo implode(", ",$item["stars"]); ?></td>
+                    <td><?php echo implode(", ",$item["star"]); ?></td>
                 </tr>
                 <?php } else if (strtolower($item["category"]) == "music") { ?>
                 <tr>
                     <th>Artist</th>
-                    <td><?php echo $item["artist"]; ?></td>
+                    <td><?php echo implode(", ",$item["artist"]); ?></td>
                 </tr>
                 <?php } ?>
             </table>
--- inc/functions.php
+++ inc/functions.php
@@ -18,7 +18,7 @@ function single_item_array($id){
     $results = $db->prepare("SELECT title, category,img,format,year, genre, publisher, isbn FROM Media 
     JOIN Genres ON Media.genre_id = Genres.genre_id 
     LEFT OUTER JOIN Books ON Media.media_id = Books.media_id 
-    Where Media.media_id = ?");
+    WHERE Media.media_id = ?");
     $results->bindParam(1,$id,PDO::PARAM_INT);
     $results->execute();
   }catch(Exception $e){
@@ -26,8 +26,24 @@ function single_item_array($id){
     exit;
   }
   //FETCH DATA ASSOCIATIVE
-  $catalog = $results->fetch();
-  return $catalog;
+  $item = $results->fetch();
+  if(empty($item)){
+    return $item;
+  }
+  try{
+    $results = $db->prepare("SELECT fullname, role FROM Media_People
+    JOIN People ON Media_People.people_id = People.people_id 
+    WHERE Media_People.media_id = ?");
+    $results->bindParam(1,$id,PDO::PARAM_INT);
+    $results->execute();
+  }catch(Exception $e){
+    echo "Unable to retrieved results";
+    exit;
+  }
+  while($row=$results->fetch(PDO::FETCH_ASSOC)){
+    $item[$row["role"]][] = $row["fullname"];
+  }
+  return $item;
 }
 function get_item_html($id,$item) {
     $output = "<li><a href='details.php?id="
